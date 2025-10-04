611c45827083f8dd4f958ff921312111f1e2a9de
sys-thai69 <cpheav@paragoniu.edu.kh>
2025-10-05 00:59:56 +0700

    small fix

--- projects/hangman.py
+++ projects/hangman.py
@@ -1,21 +1,34 @@
 import random
 
+#Setting up the game
 random_word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(random_word_list)
 word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 #(f'Pssst, the solution is {chosen_word}.')
+
+#Display here is a list of "_" representing each letter in the chosen word
 display = []
+
+#Create blanks that have equal length to the chosen word
 for _ in range(word_length):
     display += "_"
+  
+#make it possible to end the game
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+    #Check guessed letter, no live will be lost if the letter was already guessed
+    #But a message will be displayed
     if guess in display:
         print(f"You've already guessed {guess}")
+
+
+    #Here we check if the guessed letter is in a  index position in the chosen word
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
+            #if the letter is in the chosen word, we replace the "_" in display with the letter
             display[position] = letter
     if guess not in chosen_word:
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
@@ -23,6 +36,7 @@ while not end_of_game:
         if lives == 0:
             end_of_game = True
             print("You lose.")
+    #Display the current state of the word
     print(f"{' '.join(display)}")
     if "_" not in display:
         end_of_game = True
