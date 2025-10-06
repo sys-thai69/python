1175477a0022f27ea23655c4a79de71f613da4d7
sys-thai69 <cpheav@paragoniu.edu.kh>
2025-10-06 23:35:16 +0700

    change

--- projects/blackjack_game.py
+++ projects/blackjack_game.py
@@ -3,3 +3,13 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 computer_cards = []
 user_cards = []
 def deal_card():
+  card = random.choice(cards)
+  return card
+
+def calculate_score(cards):
+  if sum(cards) == 21 and len(cards) == 2:
+    return 0
+  if 11 in cards and sum(cards) > 21:
+    cards.remove(11)
+    cards.append(1)
+  return sum(cards)
